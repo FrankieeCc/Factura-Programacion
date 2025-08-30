ff29a9c679d654da5af69f08b2a286116fd74923
FrankieCc <fcorrea@unicesar.edu.co>
2025-08-30 12:05:45 -0500

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module unicesar.edu.co.facturaprogramacion {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens unicesar.edu.co.facturaprogramacion to javafx.fxml;
+    exports unicesar.edu.co.facturaprogramacion;
+}
---
+++ src/main/java/unicesar/edu/co/facturaprogramacion/HelloController.java
@@ -0,0 +1,14 @@
+package unicesar.edu.co.facturaprogramacion;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
